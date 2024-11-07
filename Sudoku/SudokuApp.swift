d7d103fc3d0f2368d31b0887cb598258ea909add
jaredcassoutt <jaredcassoutt@gmail.com>
2024-11-07 15:33:32 -0800

    Initial Commit

---
+++ Sudoku/SudokuApp.swift
@@ -0,0 +1,17 @@
+//
+//  SudokuApp.swift
+//  Sudoku
+//
+//  Created by Jared Cassoutt on 11/7/24.
+//
+
+import SwiftUI
+
+@main
+struct SudokuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SudokuTests/SudokuTests.swift
@@ -0,0 +1,17 @@
+//
+//  SudokuTests.swift
+//  SudokuTests
+//
+//  Created by Jared Cassoutt on 11/7/24.
+//
+
+import Testing
+@testable import Sudoku
+
+struct SudokuTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
